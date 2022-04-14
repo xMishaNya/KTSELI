37204c77d23e9e49fd1d37bbc3dfc7138d8ba1b9
Maner00 <zaripov-sanya@mail.ru>
2022-04-14 11:07:07 +1000

    Файлы проекта

--- ktseli/webpack.mix.js
+++ ktseli/webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+
+
+mix.postCss(('resources/css/main.css', 'resources/css/font-awesome.css', 'resources/css/styles.css'), 'public/css');
