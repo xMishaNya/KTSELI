c21190375819c502c9c40aa2906211491a044d1c
Maner00 <zaripov-sanya@mail.ru>
2022-04-16 21:20:39 +1000

    Добавление страниц логина и регистрации

---
+++ ktseli/resources/views/registration.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSRF TOKEN -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>Taskman</title>
+  <!-- Styles -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<h1>Регистрация</h1>
+<form class="col-3 offset-4 border rounded" method="POST" action="{{ route('user.registration') }}">
+  @csrf
+  <div class="form-group">
+    <label for="email" class="col-form-label-lg">Ваш email</label>
+    <input class="form-control" id="email" name="email" type="text"  value="" placeholder="Email">
+    @error('email')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+  </div>
+  <div class="form-group">
+    <label for="password" class="col-form-label-lg">Пароль</label>
+    <input class="form-control" id="password" name="password" type="password" value="" placeholder="Пароль:">
+    @error('password')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+  </div>
+  <div class="form-group">
+    <button class="btn btn-lg btn-primary" type="submit" name="sendMe" value="1">Войти</button>
+  </div>
+</form>
+
+</body>
+</html>
